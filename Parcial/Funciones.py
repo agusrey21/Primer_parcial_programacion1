a3f1fad5f5203e8f74a964346a6d4a4f50ac4f28
Agustin Rey <agustinrey419@gmail.com>
2025-06-09 09:26:41 -0300

    Primer commit de mi parcial

---
+++ Parcial/Funciones.py
@@ -0,0 +1,131 @@
+def promedio(lista):
+    suma = 0
+    for i in range(len(lista)):
+        suma += lista[i]
+    return suma / len(lista)
+
+def promedio_jurado(matriz, indice_jurado):
+    suma = 0
+    for i in range(len(matriz)):
+        suma += matriz[i][indice_jurado]
+    return suma / len(matriz)
+
+def mostrar_participantes(nombres, matriz):
+    print('--- Puntajes ---')
+    for i in range(len(nombres)):
+        print(nombres[i])
+        for j in range(len(matriz[i])):
+            print(f'Jurado N°{j+1}: {matriz[i][j]} ')
+        prom = promedio(matriz[i])
+        print(f'Promedio: {prom}')
+    return True
+
+def mostrar_menores_a(nombres, matriz, limite):
+    hay = False
+    print()
+    print("--- Participantes con promedio menor a", limite, "---")
+    for i in range(len(nombres)):
+        prom = promedio(matriz[i])
+        if prom < limite:
+            print(nombres[i], 'Promedio:', prom)
+            hay = True
+    print()
+    return hay
+
+def buscar_nombre(nombres, matriz, nombre_buscado):
+    for i in range(len(nombres)):
+        if nombres[i].lower() == nombre_buscado.lower():
+            print(f"Se busco al participante: {nombres[i]}")
+            print("Sus putajes fueron los siguientes: ")
+            for j in range(len(matriz[i])):
+                print(f"Jurado N°{j+1}: {matriz[i][j]}")
+            prom = promedio(matriz[i])
+            print(f"Su promedio fue de: {prom}")
+            return True  
+    return False  
+
+
+def burbujeo(valores):
+    indices = list(range(len(valores)))
+    for i in range(len(valores)):
+        for j in range(i + 1, len(valores)):
+            if valores[j] > valores[i]:
+                valores[i], valores[j] = valores[j], valores[i]
+                indices[i], indices[j] = indices[j], indices[i]
+    return indices, valores
+
+def top_3(nombres, matriz):
+    promedios = []
+    for i in range(len(nombres)):
+        prom = promedio(matriz[i])
+        promedios += [prom]
+    indices, ordenados = burbujeo(promedios)
+    print("\n--- Top 3 Participantes ---")
+    for k in range(3):
+        print(nombres[indices[k]], "- Promedio:", ordenados[k])
+
+def ordenar_alfabeticamente(nombres, matriz):
+    for i in range(len(nombres)):
+        for j in range(i + 1, len(nombres)):
+            if nombres[i].lower() > nombres[j].lower():
+                nombres[i], nombres[j] = nombres[j], nombres[i]
+                matriz[i], matriz[j] = matriz[j], matriz[i]
+    return nombres, matriz
+
+def jurado_mas_estricto(matriz):
+    jurados = len(matriz[0])
+    promedios = []
+    for j in range(jurados):
+        prom = promedio_jurado(matriz, j)
+        promedios += [prom]
+    minimo = promedios[0]
+    resultado = "Jurado N° 1"
+    for j in range(1, jurados):
+        if promedios[j] < minimo:
+            minimo = promedios[j]
+            resultado = "Jurado N°" + str(j+1)
+        elif promedios[j] == minimo:
+            resultado += ", Jurado N°" + str(j+1)
+    return resultado
+
+def jurado_mas_generoso(matriz):
+    jurados = len(matriz[0])
+    promedios = []
+    for j in range(jurados):
+        prom = promedio_jurado(matriz, j)
+        promedios += [prom]
+    maximo = promedios[0]
+    resultado = "Jurado N° 1"
+    for j in range(1, jurados):
+        if promedios[j] > maximo:
+            maximo = promedios[j]
+            resultado = "Jurado N°" + str(j+1)
+        elif promedios[j] == maximo:
+            resultado += ", Jurado N°" + str(j+1)
+    return resultado
+
+def participantes_con_puntajes_iguales(nombres, matriz):
+    resultados = []
+
+    for i in range(len(matriz)):
+        iguales = True
+        primero = matriz[i][0]
+        for j in range(1, len(matriz[i])):
+            if matriz[i][j] != primero:
+                iguales = False
+        if iguales:
+            resultados += [(nombres[i], primero)]
+
+    return resultados
+
+def mostrar_ganador(matriz):
+    mayor = promedio(matriz[0])
+    ganadores = [0]
+    for i in range(1, len(matriz)):
+        p = promedio(matriz[i])
+        if p > mayor:
+            mayor = p
+            ganadores = [i]
+        elif p == mayor:
+            ganadores += [i]
+    return ganadores
---
+++ Parcial/Main.py
@@ -0,0 +1,128 @@
+import Input
+import Funciones
+
+nombres = []
+matriz_puntajes = []
+
+while True:
+    print('---- MENU ----')
+    print('1. Cargar participantes')
+    print('2. Cargar puntuaciones')
+    print('3. Mostrar puntuaciones')
+    print('4. Participantes con promedio menor a 4')
+    print('5. Participantes con promedio menor a 8')
+    print('6. Buscar participante por nombre')
+    print('7. Top 3 mejores promedios')
+    print('8. Participantes ordenados alfabeticamente')
+    print('9. Jurado mas estricto')
+    print('10. Jurado mas generoso')
+    print('11. Participantes con puntuaciones iguales')
+    print('12. Mostrar ganador')
+    print('0. Salir')
+
+    opcion = input('Opcion: ')
+
+    if opcion == '1':
+        nombres = Input.carga_participantes()
+
+    elif opcion == '2':
+        if nombres == []:
+            print('Primero se deben cargar los nombres.')
+        else:
+            matriz_puntajes = Input.carga_puntajes(nombres)
+
+    elif opcion == '3':
+        if matriz_puntajes:
+            booleano = Funciones.mostrar_participantes(nombres, matriz_puntajes)
+            if booleano == True:
+                print()
+                print("Se mostraron todos los participantes.")
+        else:
+            print('Primero carga los puntajes.')
+
+    elif opcion == '4':
+        if matriz_puntajes:
+            resultado = Funciones.mostrar_menores_a(nombres, matriz_puntajes, 4)
+            if not resultado:
+                print('No hay participantes con promedio menor a', 4)
+        else:
+            print('Primero carga los puntajes.')
+
+    elif opcion == '5':
+        if matriz_puntajes:
+            hay = Funciones.mostrar_menores_a(nombres, matriz_puntajes, 8)
+            if not hay:
+                print('No hay participantes con promedio menor a', 8)
+        else:
+            print('Primero carga los puntajes.')
+
+    elif opcion == '6':
+        if matriz_puntajes:
+            nombre = input('Nombre a buscar: ')
+            esta = Funciones.buscar_nombre(nombres, matriz_puntajes, nombre)
+            if esta == False:
+                print('Participante no encontrado')
+        else:
+            print('Primero carga los puntajes')
+
+
+    elif opcion == '7':
+        if matriz_puntajes:
+            Funciones.top_3(nombres, matriz_puntajes)
+        else:
+            print('Primero carga los puntajes.')
+
+
+    elif opcion == '8':
+        if matriz_puntajes:
+            nombres, matriz_puntajes = Funciones.ordenar_alfabeticamente(nombres, matriz_puntajes)
+            Funciones.mostrar_participantes(nombres, matriz_puntajes)
+        else:
+            print('Primero carga los puntajes.')
+
+
+    elif opcion == '9':
+        if matriz_puntajes:
+            jurado_estricto = Funciones.jurado_mas_estricto(matriz_puntajes)
+            print(f'jurado(s) mas estricto(s): {jurado_estricto}')
+        else:
+            print('Primero carga los puntajes.')
+
+    elif opcion == '10':
+        if matriz_puntajes:
+            jurado_generoso = Funciones.jurado_mas_generoso(matriz_puntajes)
+            print(f'jurado(s) mas generoso(s): {jurado_generoso}')
+        else:
+            print('Primero carga los puntajes.')
+
+    elif opcion == '11':
+        if matriz_puntajes:
+            iguales = Funciones.participantes_con_puntajes_iguales(nombres, matriz_puntajes)
+        
+            if iguales:
+                for nombre, puntos in iguales:
+                    print(f"{nombre} tiene puntajes iguales de todos los jurados: {puntos}")
+            else:
+                print("No hay participantes con puntajes iguales entre todos los jurados.")
+        else:
+            print('Primero carga los puntajes.')
+    
+    elif opcion == "12":
+        if matriz_puntajes:
+            ganadores = Funciones.mostrar_ganador(matriz_puntajes)
+            if len(ganadores) == 1:
+                print(f"El ganador es: {nombres[ganadores[0]]}")
+            else:
+                print("Empate entre:")
+                for i in ganadores:
+                    print(nombres[i])
+        else:
+            print('Primero carga los puntajes.')
+
+
+    elif opcion == '0':
+        print('Fin del programa.')
+        break
+
+    else:
+        print('Opcion invalida.')
